cba3309f9906ed52819806def77809da3118256c
Yosef Moatti <moatti@il.ibm.com>
2023-12-07 10:28:32 -0600

    Modified so that both kserve-demo-http and kserver-demo-grpc may be removed

--- demo/kserve/scripts/test/delete-model.sh
+++ demo/kserve/scripts/test/delete-model.sh
@@ -4,8 +4,24 @@ set -o nounset
 set -o errtrace
 # set -x   #Uncomment this to debug script.
 
-source "$(dirname "$(realpath "$0")")/../env.sh"
+### This script will remove, if relevant, both kserve-demo-http and kserve-demo-grpc namespaces and their content and then will remove the minio namespace
 
-oc delete isvc,pod --all -n ${TEST_NS} --force --grace-period=0
-oc delete ns ${TEST_NS} ${MINIO_NS} --force --grace-period=0
+source "$(dirname "$(realpath "$0")")/../env.sh"
+export TEST_NS_HTTP=${TEST_NS}"-http"
+export TEST_NS_GRPC=${TEST_NS}"-grpc"
+
+oc get ns ${TEST_NS_HTTP}}
+if [[ $? ==  0 ]]
+then
+    oc delete isvc,pod --all -n ${TEST_NS_HTTP} --force --grace-period=0
+fi
+
+oc get ns ${TEST_NS_GRPC}}
+if [[ $? ==  0 ]]
+then
+    oc delete isvc,pod --all -n ${TEST_NS_GRPC} --force --grace-period=0
+fi
+
+### common to all protocols:
+oc delete ns ${MINIO_NS} --force --grace-period=0
 
