4f6559a9c80a04db550d45d936be5370d3676ff9
MOATTI@il.ibm.com <moatti@util-srv-2.sl.cloud9.ibm.com>
2023-12-07 10:10:49 -0600

    One mandatary arg the protocol, either http or grpc. The isvc and the namespace will be specific to the protocol name

--- demo/kserve/scripts/test/deploy-model.sh
+++ demo/kserve/scripts/test/deploy-model.sh
@@ -4,7 +4,24 @@ set -o nounset
 set -o errtrace
 # set -x   #Uncomment this to debug script.
 
+# Usage: a single! arg: "http" or "grpc" - the protocol to be used 
+
+# Check if a single argument is passed
+if [ "$#" -ne 1 ]; then
+    echo "Error: exactly one argument is required: either 'http' or 'grpc'"
+    exit 1
+fi
+
+# Check if the argument is either "http" or "grpc"
+if [ "$1" = "http" ] || [ "$1" = "grpc" ]; then
+    INF_PROTO=$1
+else
+    echo "Error: Argument must be either 'http' or 'grpc'."
+    exit 1
+fi
+
 source "$(dirname "$(realpath "$0")")/../env.sh"
+export TEST_NS=${TEST_NS}"-$INF_PROTO"
 
 # Deploy Minio
 ACCESS_KEY_ID=THEACCESSKEY
@@ -30,18 +47,23 @@ if [[ $? ==  1 ]]
 then
   oc new-project ${TEST_NS}
   
-  oc apply -f ./custom-manifests/caikit/caikit-tgis-servingruntime.yaml -n ${TEST_NS}
+  oc apply -f ./custom-manifests/caikit/caikit-tgis-servingruntime-"$INF_PROTO".yaml -n ${TEST_NS}
 
   oc apply -f ${BASE_DIR}/minio-secret-current.yaml -n ${TEST_NS} 
   oc apply -f ${BASE_DIR}/serviceaccount-minio-current.yaml -n ${TEST_NS}
 
-  oc apply -f ./custom-manifests/caikit/caikit-tgis-isvc.yaml -n ${TEST_NS}
+  ###  create the isvc.   First step: create the yaml file
+  ISVC_NAME=caikit-tgis-isvc-"$INF_PROTO"
+  sed "s/<protocol>/$INF_PROTO/g" ./custom-manifests/caikit/caikit-tgis-isvc-template.yaml > ./custom-manifests/caikit/"$ISVC_NAME".yaml
+  oc apply -f ./custom-manifests/caikit/"$ISVC_NAME".yaml -n ${TEST_NS}
 
   # Resources needed to enable metrics for the model 
   # The metrics service needs the correct label in the `matchLabel` field. The expected value of this label is `<isvc-name>-predictor-default`
-  # The metrics service in this repo is configured to work with the example model. If you are deploying a different model or using a different model name, change the label accordingly. 
-  oc apply -f custom-manifests/metrics/caikit-metrics-service.yaml -n ${TEST_NS}
-  oc apply -f custom-manifests/metrics/caikit-metrics-servicemonitor.yaml -n ${TEST_NS}
+  # The metrics service in this repo is configured to work with the example model. If you are deploying a different model or using a different model name, change the label accordingly.
+
+  ### TBD: Following 2 line should take into account the changed names 
+  # oc apply -f custom-manifests/metrics/caikit-metrics-service.yaml -n ${TEST_NS}
+  # oc apply -f custom-manifests/metrics/caikit-metrics-servicemonitor.yaml -n ${TEST_NS}
 else
   echo 
   echo "* ${TEST_NS} exist. Please remove the namespace or use another namespace name"
