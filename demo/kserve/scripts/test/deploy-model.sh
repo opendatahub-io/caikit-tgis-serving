07f3dbd0bb63d496971a81e8dc05af871be69271
Yosef Moatti <moatti@il.ibm.com>
2023-12-12 10:47:47 -0600

    Move to single namespace: kserve-demo, HTTP becomes default

--- demo/kserve/scripts/test/deploy-model.sh
+++ demo/kserve/scripts/test/deploy-model.sh
@@ -4,24 +4,30 @@ set -o nounset
 set -o errtrace
 # set -x   #Uncomment this to debug script.
 
-# Usage: a single! arg: "http" or "grpc" - the protocol to be used 
+# Deploys model for HTTP (default) or gRPC if "grpc" is passed as argument
 
-# Check if a single argument is passed
-if [ "$#" -ne 1 ]; then
-    echo "Error: exactly one argument is required: either 'http' or 'grpc'"
+# Check if at most one argument is passed
+if [ "$#" -gt 1 ]; then
+    echo "Error: at most a single argument ('http' or 'grpc') or no argument, default protocol being 'http'"
     exit 1
 fi
 
-# Check if the argument is either "http" or "grpc"
-if [ "$1" = "http" ] || [ "$1" = "grpc" ]; then
-    INF_PROTO=$1
-else
-    echo "Error: Argument must be either 'http' or 'grpc'."
-    exit 1
+# Default values that fit the default 'http' protocol:
+INF_PROTO=""
+
+# If we have an argument, check that it is either "http" or "grpc"
+if [ "$#" -eq 1 ]; then
+    if [ "$1" = "http" ]; then
+	:  ### nothing to be done
+    elif [ "$1" = "grpc" ]; then
+	INF_PROTO="-grpc"
+    else
+	echo "Error: Argument must be either 'http' or 'grpc'."
+	exit 1
+    fi
 fi
 
 source "$(dirname "$(realpath "$0")")/../env.sh"
-export TEST_NS=${TEST_NS}"-$INF_PROTO"
 
 # Deploy Minio
 ACCESS_KEY_ID=THEACCESSKEY
@@ -41,29 +47,28 @@ else
 fi
 sed "s/<minio_ns>/$MINIO_NS/g" ./custom-manifests/minio/serviceaccount-minio.yaml | tee ${BASE_DIR}/serviceaccount-minio-current.yaml 
 
-# Deploy a sample model
+# Test if ${TEST_NS} namespace already exists:
 oc get ns ${TEST_NS}
 if [[ $? ==  1 ]]
 then
-  oc new-project ${TEST_NS}
-  
-  oc apply -f ./custom-manifests/caikit/caikit-tgis-servingruntime-"$INF_PROTO".yaml -n ${TEST_NS}
+    oc new-project ${TEST_NS}
+   
+    oc apply -f ./custom-manifests/caikit/caikit-tgis-servingruntime"${INF_PROTO}".yaml -n ${TEST_NS}
 
-  oc apply -f ${BASE_DIR}/minio-secret-current.yaml -n ${TEST_NS} 
-  oc apply -f ${BASE_DIR}/serviceaccount-minio-current.yaml -n ${TEST_NS}
+    oc apply -f ${BASE_DIR}/minio-secret-current.yaml -n ${TEST_NS} 
+    oc apply -f ${BASE_DIR}/serviceaccount-minio-current.yaml -n ${TEST_NS}
 
-  ###  create the isvc.   First step: create the yaml file
-  ISVC_NAME=caikit-tgis-isvc-"$INF_PROTO"
-  sed "s/<protocol>/$INF_PROTO/g" ./custom-manifests/caikit/caikit-tgis-isvc-template.yaml > ./custom-manifests/caikit/"$ISVC_NAME".yaml
-  oc apply -f ./custom-manifests/caikit/"$ISVC_NAME".yaml -n ${TEST_NS}
+    ###  create the isvc.   First step: create the yaml file
+    ISVC_NAME=caikit-tgis-isvc"${INF_PROTO}"
+    oc apply -f ./custom-manifests/caikit/"$ISVC_NAME".yaml -n ${TEST_NS}
 
-  # Resources needed to enable metrics for the model 
-  # The metrics service needs the correct label in the `matchLabel` field. The expected value of this label is `<isvc-name>-predictor-default`
-  # The metrics service in this repo is configured to work with the example model. If you are deploying a different model or using a different model name, change the label accordingly.
+    # Resources needed to enable metrics for the model 
+    # The metrics service needs the correct label in the `matchLabel` field. The expected value of this label is `<isvc-name>-predictor-default`
+    # The metrics service in this repo is configured to work with the example model. If you are deploying a different model or using a different model name, change the label accordingly.
 
-  ### TBD: Following 2 line should take into account the changed names 
-  # oc apply -f custom-manifests/metrics/caikit-metrics-service.yaml -n ${TEST_NS}
-  # oc apply -f custom-manifests/metrics/caikit-metrics-servicemonitor.yaml -n ${TEST_NS}
+    ### TBD: Following 2 line should take into account the changed names 
+    # oc apply -f custom-manifests/metrics/caikit-metrics-service.yaml -n ${TEST_NS}
+    # oc apply -f custom-manifests/metrics/caikit-metrics-servicemonitor.yaml -n ${TEST_NS}
 else
   echo 
   echo "* ${TEST_NS} exist. Please remove the namespace or use another namespace name"
