926b4a97b914306e313d7f8741a0e428498611bb
Yosef Moatti <moatti@il.ibm.com>
2023-12-12 10:37:21 -0600

    delete-model.sh reverted to original version - no change for PR 183

--- demo/kserve/scripts/test/delete-model.sh
+++ demo/kserve/scripts/test/delete-model.sh
@@ -4,30 +4,8 @@ set -o nounset
 set -o errtrace
 # set -x   #Uncomment this to debug script.
 
-### This script will remove objects related to the protocol specified (http or grpc) as single and mandatory parameter
-
-# Check if a single argument is passed
-if [ "$#" -ne 1 ]; then
-    echo "Error: exactly one argument is required: either 'http' or 'grpc'"
-    exit 1
-fi
-
-# Check if the argument is either "http" or "grpc"
-if [ "$1" = "http" ] || [ "$1" = "grpc" ]; then
-    INF_PROTO=$1
-else
-    echo "Error: Argument must be either 'http' or 'grpc'."
-    exit 1
-fi
-
 source "$(dirname "$(realpath "$0")")/../env.sh"
-export TEST_NS_REMOVE=${TEST_NS}"-"${INF_PROTO}
 
-oc get ns ${TEST_NS_REMOVE}
-if [[ $? ==  0 ]]
-then
-    oc delete isvc,pod --all -n ${TEST_NS_REMOVE} --force --grace-period=0
-    oc delete ns  ${TEST_NS_REMOVE} --force --grace-period=0
-fi
-
+oc delete isvc,pod --all -n ${TEST_NS} --force --grace-period=0
+oc delete ns ${TEST_NS} ${MINIO_NS} --force --grace-period=0
 
