c77e736fa2fecfd637c6d81158da888c6be59cf8
Yosef Moatti <moatti@il.ibm.com>
2023-12-13 10:57:58 -0600

    consistent use of brackets around env variables

--- demo/kserve/scripts/test/grpc-call.sh
+++ demo/kserve/scripts/test/grpc-call.sh
@@ -14,13 +14,13 @@ echo "Wait until grpc runtime is READY"
 
 ISVC_NAME=caikit-tgis-isvc-grpc
 wait_for_pods_ready "serving.kserve.io/inferenceservice=${ISVC_NAME}" "${TEST_NS}"
-oc wait --for=condition=ready pod -l serving.kserve.io/inferenceservice=$ISVC_NAME -n ${TEST_NS} --timeout=300s
+oc wait --for=condition=ready pod -l serving.kserve.io/inferenceservice=${ISVC_NAME} -n ${TEST_NS} --timeout=300s
 
 echo
 echo "Testing all token in a single call"
 echo
 
-export KSVC_HOSTNAME=$(oc get ksvc "$ISVC_NAME"-predictor -n ${TEST_NS} -o jsonpath='{.status.url}' | cut -d'/' -f3)
+export KSVC_HOSTNAME=$(oc get ksvc "${ISVC_NAME}"-predictor -n ${TEST_NS} -o jsonpath='{.status.url}' | cut -d'/' -f3)
 
 ### Invoke the inferences:
 grpcurl -insecure -d '{"text": "At what temperature does Nitrogen boil?"}' -H "mm-model-id: flan-t5-small-caikit" ${KSVC_HOSTNAME}:443 caikit.runtime.Nlp.NlpService/TextGenerationTaskPredict
